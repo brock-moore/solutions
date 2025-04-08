4bc933e3e7383157465ce18638df2132dde64d01
brock-moore <61211638+brock-moore@users.noreply.github.com>
2025-04-07 22:01:26 -0500

    Update valid_anagram.py

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,4 +1,4 @@
-def isAnagram(self, s: str, t: str) -> bool:
+def isAnagram(s: str, t: str) -> bool:
         # Create char arrays, sort, and compare
         # Time O(nlogn) for sort time where n = len(s + t)
         # Space O(n) for lists where n = len(s + t)
@@ -11,3 +11,27 @@ def isAnagram(self, s: str, t: str) -> bool:
         chars.sort()
         chars2.sort()
         return chars == chars2
+
+def isAnagramHash(s: str, t: str) -> bool:
+        # Hashmap with char counts
+        # Time O(n) loop through strings three times with constant time Dict ops
+        # Space O(n) created two dicts of roughly size s and t
+        if len(s) != len(t):
+            return False
+        sDict = {}
+        for char in s:
+            if sDict.get(char):
+                sDict[char] += 1
+            else:
+                sDict[char] = 1
+        tDict = {}
+        for char in t:
+            if tDict.get(char):
+                tDict[char] += 1
+            else:
+                tDict[char] = 1
+        # Loop through set of characters to check equal counts
+        for i in s:
+            if tDict.get(i) is None or sDict[i] != tDict[i]:
+                return False
+        return True
